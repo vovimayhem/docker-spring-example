9f85df4b11615dd05abbaf390be3d12912cc4a5b
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-04-22 17:57:25 -0500

    Initial Commit

---
+++ src/main/java/com/example/demo/CustomerController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class CustomerController {
+
+  @GetMapping("/customer")
+  public String customerForm(Model model) {
+    model.addAttribute("customer", new Customer());
+    return "customer";
+  }
+
+  @PostMapping("/customer")
+  public String customerSubmit(@ModelAttribute Customer customer) {
+    return "customer";
+  }
+
+}
