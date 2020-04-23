b1970a0a77b9300c9b8b511d7768d89c6cfa2e5e
Roberto Quintanilla <roberto.quintanilla@gmail.com>
2020-04-22 19:02:31 -0500

    Added the database service to the dev project

---
+++ db/init/001_create_database.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE DATABASE demo_development;
+    CREATE DATABASE demo_test;
+EOSQL
